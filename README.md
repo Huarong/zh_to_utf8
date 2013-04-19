0f39d05a65b7632df6e45442e1fc3d08e98666b5
Huo Huarong <huohuarong@gmail.com>
2013-04-19 17:36:07 +0800

    add readme.md and modify file comment

---
+++ README.md
@@ -0,0 +1,5 @@
+This is a tool to convert any encoding format to 'utf-8'.
+
+## How to use:
+
+    $ python zh_to_utf8.py filename.
--- zh_to_uft8.py
+++ zh_to_uft8.py
@@ -1,6 +1,8 @@
-# This is a tool to convert any encoding format to 'utf-8'.
-# How to use:
-# $ python zh_to_utf8.py filename.
+'''
+This is a tool to convert any encoding format to 'utf-8'.
+How to use:
+$ python zh_to_utf8.py filename.
+'''
 
 
 import os.path
